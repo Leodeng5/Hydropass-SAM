d96c05f069db32f08ea792ebde4e7a5df0ad212e
Leo Deng <leodeng5@gmail.com>
2023-04-20 17:41:03 -0700

    add s3 image upload, add get-my-listings (#1)

--- src/handlers/index.mjs
+++ src/handlers/index.mjs
@@ -4,9 +4,11 @@ import { getAllUsers } from "./users/get-all-users.mjs";
 import { putUser } from "./users/put-user.mjs";
 import { putListing } from "./listings/put-listing.mjs";
 import { getAllListings } from "./listings/get-all-listings.mjs";
+import { getMyListings } from "./listings/get-my-listings.mjs";
 
 export const rootHandler = addCorsHeader(rootFunction);
 export const getAllUsersHandler = addCorsHeader(getAllUsers);
 export const putUserHandler = addCorsHeader(putUser);
 export const putListingHandler = addCorsHeader(putListing);
 export const getAllListingsHandler = addCorsHeader(getAllListings);
+export const getMyListingsHandler = addCorsHeader(getMyListings);
