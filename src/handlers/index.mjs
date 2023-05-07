0739febe454123f4db5e4afa22bed13a408b06a7
Leo Deng <leodeng5@gmail.com>
2023-05-07 13:06:18 -0700

    add GET /users/{id}

--- src/handlers/index.mjs
+++ src/handlers/index.mjs
@@ -1,6 +1,7 @@
 import addCorsHeader from "../middleware/addCorsHeader.mjs";
 import { rootFunction } from "./root.mjs";
 import { getAllUsers } from "./users/get-all-users.mjs";
+import { getUserById } from "./users/get-user-by-id.mjs";
 import { putUser } from "./users/put-user.mjs";
 import { putListing } from "./listings/put-listing.mjs";
 import { getAllListings } from "./listings/get-all-listings.mjs";
@@ -10,6 +11,7 @@ import { getFeaturedListings } from "./listings/get-featured-listings.mjs";
 
 export const rootHandler = addCorsHeader(rootFunction);
 export const getAllUsersHandler = addCorsHeader(getAllUsers);
+export const getUserByIdHandler = addCorsHeader(getUserById);
 export const putUserHandler = addCorsHeader(putUser);
 export const putListingHandler = addCorsHeader(putListing);
 export const getAllListingsHandler = addCorsHeader(getAllListings);
--- src/handlers/users/get-user-by-id.mjs
+++ src/handlers/users/get-user-by-id.mjs
@@ -3,26 +3,25 @@ import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-const tableName = process.env.LISTINGS_TABLE;
+const tableName = process.env.USERS_TABLE;
 
 /**
- * HTTP get method to get listing from DynamoDB by id.
+ * HTTP get method to get user from DynamoDB by id.
  */
-export const getListingById = async (event) => {
+export const getUserById = async (event) => {
   if (event.httpMethod !== "GET") {
     throw new Error(
-      `getListingById only accept GET method, you tried: ${event.httpMethod}`
+      `getUserById only accept GET method, you tried: ${event.httpMethod}`
     );
   }
   // All log statements are written to CloudWatch
   console.info("received:", event);
 
-  var params = {
+  const params = {
     TableName: tableName,
-    IndexName: "ListingIdIndex",
-    KeyConditionExpression: "listing_id = :listing_id",
+    KeyConditionExpression: "user_id = :user_id",
     ExpressionAttributeValues: {
-      ":listing_id": event.pathParameters.id,
+      ":user_id": event.pathParameters.id,
     },
   };
 
@@ -31,16 +30,16 @@ export const getListingById = async (event) => {
   try {
     const data = await ddbDocClient.send(new QueryCommand(params));
     if (data.Items.length == 0) {
-      console.log("Listing not found");
+      console.log("User not found");
       response = {
         statusCode: 404,
-        body: JSON.stringify({ message: "Listing not found" }),
+        body: JSON.stringify({ message: "User not found" }),
       };
     } else {
-      const listing = data.Items[0];
+      const user = data.Items[0];
       response = {
         statusCode: 200,
-        body: JSON.stringify(listing),
+        body: JSON.stringify(user),
       };
     }
   } catch (err) {
